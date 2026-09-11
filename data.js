51225dc1a24c56730aba001a70127208e901621e
Aleksandree2 <128622958+Aleksandree2@users.noreply.github.com>
2026-09-11 23:52:12 +0400

    კატალოგის განახლება ბექოფისიდან (+1 ფოტო)

--- data.js
+++ data.js
@@ -24,10 +24,17 @@ const CATEGORIES = [
 
 const PARTS = [
   {
-    name: "5113 LP95  R18",
+    name: "RAYS 5113 Dark Gunmetal  R18",
     category: "diskebi",
     price: 2200,
     image: "images/5113-lp95-r18-mtxewhmq0kzd.jpg",
     description: "Size: 18x9.5 | Bolt Pattern: 10(114.3+120) | Offset: ET:12 | Finish: Dark Gunmetal / Lip Polish | Hub Bore: CB:73.1\nSize: 18x10.5 | Bolt Pattern: 10(114.3+120) | Offset: ET:15 | Finish: Dark Gunmetal",
+  },
+  {
+    name: "RAYS 5113 Mat Bronze R18",
+    category: "diskebi",
+    price: 2200,
+    image: "images/rays-5113-mat-bronze-r18-mtxfohpvf2oj.jpg",
+    description: "Size: 18x9.5 | Bolt Pattern: 10(114.3+120) | Offset: ET:12 | Finish: Mat Bronze / Lip Polish | Hub Bore: CB:73.1\n Size: 18x10.5 | Bolt Pattern: 10(114.3+120) | Offset: ET:15 | Finish: Mat Bronze",
   },
 ];
